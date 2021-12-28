8a7f7d0a867e0c251ffb0e98ae8dd332b695f14b
Captain Nick Lucifer* <naryal2580@gmail.com>
2021-12-28 22:01:39 +0545

    misc change

--- main.py
+++ main.py
@@ -2,7 +2,6 @@
 
 from fastapi import FastAPI
 import mysql.connector
-from mysql.connector.errors import *
 
 app = FastAPI()
 
@@ -21,7 +20,7 @@ def run_query(query):
         cursor.execute(query)
         resp = cursor.fetchall()
         return dict(resp)
-    except OperationalError as e:
+    except mysql.connector.errors.OperationalError as e:
         init_db()
         return run_query(query)
     except Exception as e:
