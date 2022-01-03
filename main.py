786f7d9d3d30d8fbc14bd994affd472c350d59b7
Captain Nick Lucifer* <naryal2580@gmail.com>
2022-01-03 15:14:42 +0545

    misc changes.

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 #!/usr/bin/env python3
 
+'''
+Vulnerable FastAPI (vFastAPI)
+
+Please issue a GET request to "/docs" in order to obtain infromation regarding endpoints within this API.
+'''
+
 from typing import Optional, Any
 import asyncio
-from fastapi import FastAPI, Request
 from faker import Faker
 import aiosqlite, random
 from hashlib import md5
@@ -10,6 +15,11 @@ from os import path, remove
 from shutil import rmtree
 from montydb import set_storage, MontyClient
 from pydantic import BaseModel
+from fastapi import FastAPI, Request
+from fastapi.responses import FileResponse, HTMLResponse
+from fastapi.openapi.utils import get_openapi
+from fastapi.openapi.docs import get_swagger_ui_oauth2_redirect_html
+from fastapi.staticfiles import StaticFiles
 
 class User(BaseModel):
     name: str
@@ -19,8 +29,26 @@ class User(BaseModel):
     password: str
     contact: str
 
+author_info = {
+    'name': 'Captain Nick Lucifer*',
+    'url': 'https://is.gd/c_nick',
+    'email': 'naryal2580@gmail.com'
+        }
+__license__ = {
+        'name': 'MIT',
+        'url': 'https://github.com/naryal2580/vfapi/blob/main/LICENSE'
+        }
 DB_FILENAME = 'vfapi'
-app, fake = FastAPI(), Faker()
+app = FastAPI(
+        title="vFastAPI",
+        version="0.01a",
+        description=__doc__,
+        contact=author_info,
+        license_info=__license__,
+        redoc_url=False,
+        docs_url=False
+        )
+fake = Faker()
 set_storage(
         repository=f'{DB_FILENAME}.nosql.db',
         storage='sqlite',
@@ -32,6 +60,25 @@ db_client = MontyClient(
         automatic_index=False,
         busy_timeout=5000
         )
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+def openapi():
+    if app.openapi_schema:
+        return app.openapi_schema
+    openapi_schema = get_openapi(
+        title="vFastAPI",
+        version="0.01a",
+        description=__doc__,
+        contact=author_info,
+        license_info=__license__,
+        routes=app.routes
+    )
+    openapi_schema["info"]["x-logo"] = {
+        "url": "static/img/logo.png"
+    }
+    # print(openapi_schema)
+    app.openapi_schema = openapi_schema
+    return app.openapi_schema
 
 async def get_sql_db():
     db = await aiosqlite.connect(database=f'{DB_FILENAME}.sql.db')
@@ -187,6 +234,40 @@ async def delete_user(username: Optional[str] = '', user: Optional[User] = None)
         db_client.vfapi.users.delete_one({'address': user.address})
         await run_sql_query(f'DELETE FROM users WHERE address = {user.address};', commit=True)
     return {'resp': '!done'}
+
+@app.get('/reset')
+def reset_page():
+    return {'resp': 'Please issue a POST request to the same endpoint in order to actually reset the database.'}
+
+@app.post('/reset')
+async def reset_database():
+    remove(f'{DB_FILENAME}.sql.db')
+    rmtree(f'{DB_FILENAME}.nosql.db')
+    await init_db()
+    return {'resp': 'done'}
+
+@app.get('/favicon.ico')
+def return_favicon():
+    return FileResponse('./static/img/favicon.ico')
+
+@app.get('/robots.txt')
+def return_robots_txt():
+    return FileResponse('./static/robots.txt')
+
+@app.get('/.well-known/security.txt')
+def security_txt():
+    return FileResponse('./static/security.txt')
+
+@app.get('/docs', include_in_schema=False)
+def return_docs():
+    return HTMLResponse('<!DOCTYPE html><html><head><link type="text/css" rel="stylesheet" href="static/swagger-ui.css"><link rel="shortcut icon" href="static/img/favicon.png"><title>' + app.title + ' - Swagger UI</title></head><body><div id="vfastapi-ui"></div><script src="static/swagger-ui-bundle.js"></script><script>const ui = SwaggerUIBundle({ url: "/openapi.json", oauth2RedirectUrl: window.location.origin + "/docs/oauth2-redirect", dom_id: "#vfastapi-ui", presets: [ SwaggerUIBundle.presets.apis, SwaggerUIBundle.SwaggerUIStandalonePreset ], layout: "BaseLayout", deepLinking: true, showExtensions: true, showCommonExtensions: true });</script></body></html>', status_code=200)
+
+@app.get('/redoc', include_in_schema=False)
+def return_redoc():
+    # TODO: Serve font locally.
+    return HTMLResponse('<!DOCTYPE html><html><head><title>' + app.title + ' - ReDoc</title><meta charset="utf-8"/><meta name="viewport" content="width=device-width, initial-scale=1"><link href="https://fonts.googleapis.com/css?family=Montserrat:300,400,700|Roboto:300,400,700" rel="stylesheet"><link rel="shortcut icon" href="static/img/favicon.png"><style>body { margin: 0; padding: 0; }</style></head><body><redoc spec-url="/openapi.json"></redoc><script src="static/redoc.standalone.js"> </script></body></html>', status_code=200)
+
+app.openapi = openapi
 
 if __name__ == '__main__':
     asyncio.run(init_db()); __import__('uvicorn').run('main:app', port=8888, reload=False)
--- run.sh
+++ run.sh
@@ -1,12 +1,19 @@
 #/bin/bash
 
-main() {
-	clear
-	rm -rf __pycache__/ vfaspi.db*
+banner () {
 	figlet -f small "vFAPI"
-	echo -e "\t\tvulnerable FastAPI"
-	echo "[+] Starting Vulnerable API"
-	python3 main.py
+}
+
+main() {
+	# clear
+	rm -rf __pycache__/ vfaspi.db*
+	banner
+	echo -e "\t\tvulnerable FastAPI\n"
+	echo "[+] Starting Vulnerable API UI <dev>"
+	cd ui && npm run build && clear; banner; npm run preview -- --host --port 8008 &
+	sleep 0.48
+	echo "[+] Starting Vulnerable API <dev>"
+	cd .. && python3 main.py --host 
 }
 
 main
